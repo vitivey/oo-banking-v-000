1bcd6470ea9abc187847a4600c805a7caa185ce3
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 03:56:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,7 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
+  attr_accessor :balance, :status
+  attr_reader :name
+  
     def initialize(name)
       @name=name
       @balance=1000
