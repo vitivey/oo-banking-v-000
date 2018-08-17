09b4881c23b848ee88515a4dcd79dabd6471cadb
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 04:01:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,13 @@
 class Transfer
   # your code here
   attr_accessor :status
-    def initialize
+  
+    def initialize(sender, receiver, status)
       @status="pending"
     end
 
     def valid?
 
     end
-    
+
 end
